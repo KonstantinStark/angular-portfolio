806af30682e37b066f446c3e79e7f94eaf69bdaf
Konstantin Stark <konstantin.stark.dev@gmail.com>
2024-12-05 13:12:36 +0100

    added about me

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,11 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { SkillSetComponent } from './skill-set/skill-set.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ColleaguesComponent } from './colleagues/colleagues.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from "../shared/footer/footer.component";
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [CommonModule, LandingPageComponent],
+  imports: [CommonModule, LandingPageComponent, AboutMeComponent, SkillSetComponent, ProjectsComponent, ColleaguesComponent, ContactComponent, FooterComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
