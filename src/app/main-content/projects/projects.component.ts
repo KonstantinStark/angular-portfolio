8019a2765304cc3e772b47a49b3933c7e6109cef
Konstantin Stark <konstantin.stark.dev@gmail.com>
2024-12-11 08:43:24 +0100

    project comp

--- src/app/main-content/projects/projects.component.ts
+++ src/app/main-content/projects/projects.component.ts
@@ -1,12 +1,34 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
 export class ProjectsComponent {
+  hoveredImage: string | null = null;
 
+  projects = [
+    {
+      name: 'Join',
+      tech: 'HTML | CSS | JavaScript | Firebase',
+      image: './assets/img/projects/join.png' 
+    },
+    {
+      name: 'El Pollo Loco',
+      tech: 'HTML | CSS | JavaScript',
+      image: './assets/img/projects/loco.png'
+    }
+  ];
+
+  showImage(imagePath: string) {
+    this.hoveredImage = imagePath;
+  }
+
+  hideImage(){
+    this.hoveredImage = null;
+  }
 }
