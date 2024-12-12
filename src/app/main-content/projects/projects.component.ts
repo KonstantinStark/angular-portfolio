747fbddf1ee35002dfebc8a242646c2a789d8795
Konstantin Stark <konstantin.stark.dev@gmail.com>
2024-12-12 10:49:15 +0100

    overlay next project btn

--- src/app/main-content/projects/projects.component.ts
+++ src/app/main-content/projects/projects.component.ts
@@ -13,6 +13,8 @@ export class ProjectsComponent {
   isOverlayVisible: boolean = false;
   selectedProject: any = null;
 
+  currentProjectIndex = 0; // Start mit dem ersten Projekt
+
   projects = [
     {
       number: '01',
@@ -21,7 +23,7 @@ export class ProjectsComponent {
       image: './assets/img/projects/join.png',
       imageOverlay: './assets/img/projects/joinOverlay.png',
       description: 'Aufgabenmanager nach dem Vorbild des Kanban-Systems. Erstellen und organisieren Sie Aufgaben mit Hilfe von Drag-and-Drop-Funktionen, weisen Sie Benutzer und Kategorien zu.',
-      skillIcons: [] = [
+      skillIcons: [
         { image: './assets/img/projects/html.png' },
         { image: './assets/img/projects/css.png' },
         { image: './assets/img/projects/js.png' },
@@ -35,7 +37,7 @@ export class ProjectsComponent {
       image: './assets/img/projects/loco.png',
       imageOverlay: './assets/img/projects/locoOverlay.png',
       description: 'Sprung-, Lauf- und Wurfspiel, das auf einem objektorientierten Ansatz basiert. Hilf Pepe, Münzen und Tabasco-Salsa zu finden, um gegen die verrückte Henne zu kämpfen.',
-      skillIcons: [] = [
+      skillIcons: [
         { image: './assets/img/projects/html.png' },
         { image: './assets/img/projects/css.png' },
         { image: './assets/img/projects/js.png' },
@@ -59,5 +61,11 @@ export class ProjectsComponent {
   closeOverlay() {
     this.isOverlayVisible = false;
     this.selectedProject = null;
+  }
+
+  nextProject() {
+    this.currentProjectIndex = (this.currentProjectIndex + 1) % this.projects.length;
+    this.selectedProject = this.projects[this.currentProjectIndex];
+    this.isOverlayVisible = true;
   }
 }
