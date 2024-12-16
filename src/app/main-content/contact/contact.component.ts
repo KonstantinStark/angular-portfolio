d3f3fe6656b01f81967d21452b3d309e4eb932e5
Konstantin Stark <konstantin.stark.dev@gmail.com>
2024-12-16 10:54:27 +0100

    contact ngSubmit

--- src/app/main-content/contact/contact.component.ts
+++ src/app/main-content/contact/contact.component.ts
@@ -1,14 +1,24 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms'; 
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators, FormsModule } from '@angular/forms'; 
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [FormsModule],
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.scss'] 
 })
 export class ContactComponent {
-  
+
+  contactData = {
+    name: "",
+    email: "",
+    message: "",
+  }
+
+  onSubmit() {
+    console.log(this.contactData);
+    
+  }
 }
