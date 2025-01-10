7e813521e6482b0c363eaa8f94a16ac0b8139ba4
Konstantin Stark <konstantin.stark.dev@gmail.com>
2025-01-10 08:45:49 +0100

    translate http loader

--- src/app/main-content/landing-page/navbar/navbar.component.ts
+++ src/app/main-content/landing-page/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-navbar',
@@ -14,4 +15,6 @@ export class NavbarComponent {
   toggleHamburgerMenu() {
     this.menuHidden = !this.menuHidden;
   }
+
+  translate = inject(TranslateService)
 }
